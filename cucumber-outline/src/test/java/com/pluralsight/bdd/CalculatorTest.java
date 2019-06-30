9d4cf319e558cabd36623feb46c8fa7c2d9713df
pradeep-charism <pradeep.charism@gmail.com>
2019-07-01 00:07:57 +0800

    cleanup

--- cucumber-outline/src/test/java/com/pluralsight/bdd/CalculatorTest.java
+++
@@ -1,8 +0,0 @@
-package com.pluralsight.bdd;
-
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-public class CalculatorTest {
-}
--- cucumber-outline/src/test/java/com/pluralsight/bdd/calculator/Calculator.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight.bdd.calculator;
-
-public class Calculator {
-    public Integer add(Integer a, Integer b) {
-        return a + b;
-    }
-}
