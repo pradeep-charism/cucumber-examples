0d6be07cfe602ebea92b6ea929213aa5c2ca6709
pradeep-charism <pradeep.charism@gmail.com>
2019-07-01 01:15:42 +0800

    code refactoring

--- cucumber-serenity/src/test/java/shop/ToastBoxAcceptanceSerenityTestSuite.java
+++ cucumber-serenity/src/test/java/shop/ToastBoxAcceptanceSerenityTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "coffee.shop"
+        glue = "shop"
 )
 public class ToastBoxAcceptanceSerenityTestSuite {
 }
